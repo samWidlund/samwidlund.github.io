2d10893e23a56ce53c442b9e22e3cf5bd5a70768
samWidlund <sam.wid415@gmail.com>
2024-09-15 13:37:04 +0200

    rotation and font size fix

--- scripts/main.js
+++ scripts/main.js
@@ -8,27 +8,8 @@ window.addEventListener("load", function() {
     const scrollLimit = mainContainer.offsetHeight;
     const fadeLimit = scrollLimit + navigateContainer.offsetHeight;
 
-
-    // TESTING
-    nameSAMUEL.classList.add("rotaded");
-    nameWIDLUND.classList.add("rotaded");
-
-    // TODO: ADD ROTATE NAME ON SMALLER SCREENS LIKE PHONE!
-
     window.addEventListener("scroll", function() {
         if (window.scrollY > scrollLimit) {
-            nameText.classList.add("hidden");
-            // nameText.classList.add("rotated"); // Add rotation class
-            // nameSAMUEL.classList.add("rotaded");
-            // nameWIDLUND.classList.add("rotaded");
-        } else {
-            nameText.classList.remove("hidden");
-            // nameText.classList.remove("rotated"); // Remove rotation class
-            // nameSAMUEL.classList.remove("rotaded");
-            // nameWIDLUND.classList.remove("rotaded");
-        }
-
-        if (window.scrollY > scrollLimit) { // CHANGE TO fadeLimit!
             nameText.classList.add("hidden");
         } else {
             nameText.classList.remove("hidden");
