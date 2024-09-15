d702e9d77eceeb8bc1b63d3edc04190555c58b8d
samWidlund <sam.wid415@gmail.com>
2024-09-15 23:28:49 +0200

    add info in resume

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,8 @@
 window.addEventListener("load", function() {
     const nameText = document.getElementsByClassName("nameHeader")[0];
     const mainContainer = document.getElementById("mainContainer")
-    const nameSAMUEL = document.getElementsByClassName("SAMUEL")[0];
-    const nameWIDLUND = document.getElementsByClassName("WIDLUND")[0];
-    const navigateContainer = document.getElementById("navigateContainer");
 
     const scrollLimit = mainContainer.offsetHeight;
-    const fadeLimit = scrollLimit + navigateContainer.offsetHeight;
 
     window.addEventListener("scroll", function() {
         if (window.scrollY > scrollLimit) {
