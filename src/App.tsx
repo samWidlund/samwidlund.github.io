a4b8ac3afcbe33003e18a7bd6ab4e37d4e946835
samWidlund <sam.wid415@gmail.com>
2026-04-01 21:09:13 +0200

    border

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
     return (
         <div className="min-h-screen flex justify-end items-center bg-black p-10">
-          <div className="w-full text-end h-auto border-t border-white p-5">
+          <div className="w-full text-end h-auto border-t-2 border-white p-5">
             <h1 className="text-3xl font-bold text-[#39FF14]">
                 samuel Widlund
             </h1>           
