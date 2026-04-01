ecfe23f4e509395e1f14d9337cf260470333f6b0
samWidlund <sam.wid415@gmail.com>
2026-04-01 23:55:24 +0200

    alignment styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 export default function App() {
     return (
-        <div className="min-h-screen flex justify-end items-center bg-black p-10">
-          <div className="w-full text-end h-auto border-t-2 border-white p-5">
-            <h1 className="text-3xl font-bold text-[#39FF14] animate-slide-in">
-                samuel Widlund
-            </h1>           
-          </div>
+      <div className="w-screen h-screen bg-[#0B0B0B] flex items-center">
+        <div className="w-full flex justify-between items-center border-b-2 border-white p-5">
+            <h1 className="text-2xl text-[#39FF14]">~/portfolio_</h1>
+            <h1 className="text-2xl font-bold text-[#39FF14] animate-slide-in">samuel Widlund</h1>
         </div>
+      </div>
     );
 }
