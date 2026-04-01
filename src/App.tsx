8202e8d3bf8b5a4f9482460e54e1a00e9152ac6e
samWidlund <sam.wid415@gmail.com>
2026-04-01 20:52:52 +0200

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 export default function App() {
     return (
-        <div className="min-h-screen flex bg-black p-10">
+        <div className="min-h-screen flex justify-end items-center bg-black p-10">
+          <div className="w-full text-end h-auto border-t border-white p-5">
             <h1 className="text-3xl font-bold text-[#39FF14]">
-                samuel widlund
-            </h1>
+                samuel Widlund
+            </h1>           
+          </div>
         </div>
     );
 }
