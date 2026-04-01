9f17c1c45622adbb95b472ad97150b5c92c3de9e
samWidlund <sam.wid415@gmail.com>
2026-04-02 01:10:04 +0200

    interest bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import { Typewriter } from './components/Typewriter'
 
 export default function App() {
     return (
-      <div className="min-h-screen bg-[#0B0B0B] flex">
+      <div className="min-h-screen bg-[#0B0B0B] flex flex-col  justify-center">
+        <div className="absolute top-0 w-full flex p-2 justify-center text-white">coding/clothing/endurance
+        </div>
         <div className="w-full flex justify-between items-center p-5">
             <h1 className="text-2xl text-[#39FF14]">
               <Typewriter text="~/portfolio" />
