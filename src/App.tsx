eda047d15f090cc0eeed57196061b788d65632d9
samWidlund <sam.wid415@gmail.com>
2026-04-01 21:11:22 +0200

    fadein animation on header

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ export default function App() {
     return (
         <div className="min-h-screen flex justify-end items-center bg-black p-10">
           <div className="w-full text-end h-auto border-t-2 border-white p-5">
-            <h1 className="text-3xl font-bold text-[#39FF14]">
+            <h1 className="text-3xl font-bold text-[#39FF14] animate-slide-in">
                 samuel Widlund
             </h1>           
           </div>
