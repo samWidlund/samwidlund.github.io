4ed6210df73ae3a969230ecde883452ab3a3b0d1
samWidlund <sam.wid415@gmail.com>
2026-04-02 09:43:32 +0200

    revert back bg color to tailwind and add bg to top bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { Typewriter } from './components/Typewriter'
 
 export default function App() {
     return (
-      <div className="min-h-screen flex flex-col  justify-center">
-        <div className="absolute top-0 w-full flex p-2 justify-center text-white">ultra@runner: ~/coding/clothing/design
+      <div className="bg-[#0B0B0B] min-h-screen flex flex-col justify-center">
+        <div className="absolute top-0 w-full flex p-2 justify-center text-white bg-[rgba(30,30,30,0.85)] border-b border-[rgba(100,100,100,0.3)] backdrop-blur-sm">
+          ultra@runner: ~/coding/clothing/design
         </div>
         <div className="w-full flex justify-between items-center p-5">
             <h1 className="text-2xl text-[#39FF14]">
