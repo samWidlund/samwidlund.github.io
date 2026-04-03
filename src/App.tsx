fb13b88bbd1c2ea1d87a3b0600c47ad70d3bfbb4
samWidlund <sam.wid415@gmail.com>
2026-04-03 13:26:14 +0200

    filesystem in landingpage

--- src/App.tsx
+++ src/App.tsx
@@ -7,19 +7,21 @@ export default function App() {
 
   return (
     <div className="min-h-screen flex flex-col font-mono text-base">
-      <div className="h-screen flex flex-col justify-center">
+      <div className="h-screen flex flex-col">
         <div className="absolute top-0 w-full flex p-2 justify-center text-white bg-[rgba(30,30,30,0.85)] backdrop-blur-sm shadow-[0_1px_0_rgba(100,100,100,0.3)]">
             ultra@runner: ~/coding/clothing/design
         </div>
-        <div className="w-full flex justify-between items-center p-5">
-            <h1 className="text-base text-[#39FF14]">
-              <Typewriter text="~/portfolio" />
-            </h1>
-            <h1 className="text-2xl font-bold text-[#39FF14] animate-slide-in">samuel Widlund</h1>
+        <div className="flex-1 flex flex-col justify-center">
+          <div className="w-full flex justify-between items-center p-5">
+              <h1 className="text-base text-[#39FF14]">
+                <Typewriter text="~/portfolio" />
+              </h1>
+              <h1 className="text-2xl font-bold text-[#39FF14] animate-slide-in">samuel Widlund</h1>
+          </div>
         </div>
-      </div>
-      <div className="p-5">
-        <FileSystem currentPath={currentPath} />
+        <div className="p-5">
+          <FileSystem currentPath={currentPath} />
+        </div>
       </div>
     </div>
   );
