0b429ed78c6b6cf5cda28622ed8856d90f5a6d70
samWidlund <sam.wid415@gmail.com>
2026-04-07 21:29:52 +0200

    sticky navbar

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ export default function App() {
   return (
     <div className="min-h-screen flex flex-col font-mono text-sm">
       <div className="h-dvh flex flex-col">
-        <div className="absolute top-0 w-full flex p-2 justify-center text-white bg-[rgba(30,30,30,0.85)] backdrop-blur-sm shadow-[0_1px_0_rgba(100,100,100,0.3)]">
+        <div className="sticky top-0 w-full flex p-2 justify-center text-white bg-[rgba(30,30,30,0.85)] backdrop-blur-sm shadow-[0_1px_0_rgba(100,100,100,0.3)]">
             ultra@runner: ~/coding/clothing/design
         </div>
         <div className="flex-1 flex flex-col justify-center">
