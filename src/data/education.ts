e135699356ae6d72176559f9430d831fdb3d0dd7
samWidlund <sam.wid415@gmail.com>
2026-04-13 05:55:32 +0200

    update education.ts

--- src/data/education.ts
+++ src/data/education.ts
@@ -12,7 +12,7 @@ export const education: Education[] = [
     degree: 'Upper secondary school engineer',
     school: 'NTI Gymnasiet Uppsala',
     period: 'Aug 2023 - Jun 2024',
-    info: ['Programming', 'Design', 'Software engineering', 'Scrum/Agile', 'Project management']
+    info: ['Programming', 'Web Development', 'Design', 'Software engineering', 'Scrum/Agile', 'Project management']
   },
   {
     hash: 'e5f6g7h',
@@ -26,6 +26,6 @@ export const education: Education[] = [
     degree: 'Technology program',
     school: 'NTI Gymnasiet Uppsala',
     period: 'Aug 2020 - Jun 2023',
-    info: ['Technical science', 'Mathematics', 'Physics']
+    info: ['Technical science', 'Mathematics', 'Physics', 'Web Development', 'Programming']
   }
 ]
