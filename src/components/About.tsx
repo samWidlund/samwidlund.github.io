db855be2dd2b130a3f47f81bd9aa76575c00dcff
samWidlund <sam.wid415@gmail.com>
2026-04-13 20:50:44 +0200

    refactor About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -8,32 +8,31 @@ export const About: FC = () => {
                     <span className="text-[#39FF14]">$</span> cat ABOUT.md
                 </div>
                 <div>
-                    <h1># About</h1>
-                     hello! I'm samuel Widlund. <br />
-                    < br/>
-                    ## Intro < br/>
-                    Diploma in software engineering. based in
+                    <p># About</p>
+                    hello! I'm samuel Widlund. <br />
+                    <br/>
+                    <p>## Intro</p>
+                    <p>Diploma in software engineering. based in
                     Uppsala, Sweden with a keen interest in software development,
                     testing and design. in addition, I spend my free time engaging in
                     ultrarunning aswell as reselling clothing. currently training for
-                    tänndalen 100 miles.
+                    tänndalen 100 miles. </p>
                     <br />
+                    <p>## Tech</p>
+                    <p>- JavaScript, TypeScript, React </p>
+                    <p>- CSS, Tailwind, Bootstrap </p>
+                    <p>- Python, C#, VBScript </p>
+                    <p>- Bash, Fish, Powershell </p>
+                    <p>- Windows, Linux </p>
+                    <p>- Azure, Pipelines, Yaml </p>
+                    <p>- SQL, SQLite </p>
+                    <p>- Selenium, FlaUI </p>
+                    <p>- Scrum, git </p>
                     <br />
-                    ## Tech <br />
-                    - JavaScript, TypeScript, React < br/>
-                    - CSS, Tailwind, Bootstrap < br/>
-                    - Python, C#, VBScript < br/>
-                    - Bash, Fish, Powershell < br/>
-                    - Windows, Linux < br/>
-                    - Azure, Pipelines, Yaml < br/>
-                    - SQL, SQLite < br/>
-                    - Selenium, FlaUI < br/>
-                    - Scrum, git < br/>
-                    < br/>
-                    ## Links < br/>
-                    Github < br/>
-                    Linkedin < br/>
-                    Mail < br/>
+                    <p>## Links</p>
+                    <p>Github </p>
+                    <p>Linkedin </p>
+                    <p>Mail </p>
 
                 </div>
             </div>
