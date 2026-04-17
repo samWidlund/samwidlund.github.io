949bee2240b7a9e602c2e75be20ef886144f9c96
samWidlund <sam.wid415@gmail.com>
2026-04-17 22:03:21 +0200

    footer: increase y padding and font size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
     return (
-        <footer className="py-2 mt-4 text-center text-xs text-white bg-[rgba(30,30,30,0.85)]">
+        <footer className="py-4 mt-4 text-center text-sm text-white bg-[rgba(30,30,30,0.85)]">
             © samuel widlund 2026
         </footer>
     );
