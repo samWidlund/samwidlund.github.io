41de04c3b6d58577f675682f00c3c9dd248536b9
samWidlund <sam.wid415@gmail.com>
2026-04-26 02:52:24 +0200

    monospace font for W

--- src/components/BigW.tsx
+++ src/components/BigW.tsx
@@ -3,7 +3,7 @@ export function BigW() {
     <div className="fixed inset-0 flex items-center justify-center pointer-events-none select-none overflow-hidden">
       <span
         className="text-[45vw] leading-none font-black text-[#FF760D] opacity-[0.03]"
-        style={{ fontFamily: 'Trebuchet MS, Impact, sans-serif' }}
+        style={{ fontFamily: 'ui-monospace, monospace' }}
       >
         W
       </span>
