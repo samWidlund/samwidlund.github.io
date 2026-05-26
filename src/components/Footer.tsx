9508099b1db3fb0e113c72bcd9e1f15997460401
Samuel Widlund <sam.wid415@gmail.com>
2026-05-26 08:46:46 +0200

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
         <footer className="py-4 mt-4 text-sm text-center text-white bg-[rgba(30,30,30,0.85)]">
             © samuel widlund 2026 <br />
             <br />
-            created using react, tailwind, & typescript
+            ( react, typescript & tailwind )
         </footer>
     );
 }
